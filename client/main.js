afca0264a0bd9cc71ca624f2f2499e663d2c06e0
Brady Wright <Pro75357@gmail.com>
2017-08-20 14:01:36 -0700

    Revert "Search By Name"
    This reverts commit 992609570b8d0ad71d7a31b471e9a3277875947a.

--- client/main.js
+++ client/main.js
@@ -6,33 +6,32 @@ import { Results } from '../imports/Results';
 import './main.html';
 
 Template.body.helpers({
-    searchTypeName(){
-        return Session.get('searchTypeName')
+    searchType(){
+        return Session.get('searchType')
     }
 })
 
 Template.searchSelect.onCreated(function searchOnCreated() {
-    Meteor.call('clearResults') // clears previous results from db on creation
-    Session.set('NPIresultPresent', false)
-    Session.set('nameResultPresent', false)
-    Session.set('searchTypeName',false)
+    Meteor.call('ClearResults') // clears previous results from db on creation
+    Session.set('resultPresent', false)
+    Session.set('searchType',false)
 })
 
 
 Template.searchSelect.events({
     'click #namebutton'(event) {
         event.preventDefault()
-        Session.set('searchTypeName', true)
+        Session.set('searchType', true)
     },
     'click #NPIbutton'(event) {
         event.preventDefault()
-        Session.set('searchTypeName', false)
+        Session.set('searchType', false)
     }
 })
 
 Template.NPIresults.helpers({
-    NPIresultPresent() {
-        return Session.get('NPIresultPresent')
+    resultPresent() {
+        return Session.get('resultPresent')
     },
 
     NPI() {
@@ -40,25 +39,23 @@ Template.NPIresults.helpers({
     },
 
     firstname() {
-        return Results.findOne({type: 'npi'}).result.results[0].basic.first_name
+        return Results.findOne({}).result.results[0].basic.first_name
     },
     lastname() {
-        return Results.findOne({ type: 'npi'}).result.results[0].basic.last_name
+        return Results.findOne({}).result.results[0].basic.last_name
     },
     credential() {
-        return Results.findOne({ type: 'npi'}).result.results[0].basic.credential
+        return Results.findOne({}).result.results[0].basic.credential
     },
     city() {
-        return Results.findOne({ type: 'npi'}).result.results[0].addresses[0].city
+        return Results.findOne({}).result.results[0].addresses[0].city
     },
     state() {
-        return Results.findOne({ type: 'npi' }).result.results[0].addresses[0].state
-    },
-    lastUpdated() {
-        return Results.findOne({ type: 'npi'}).result.results[0].basic.last_updated
+        return Results.findOne({}).result.results[0].addresses[0].state
     },
     taxonomies() {
-        return Results.findOne({ type: 'npi' }).result.results[0].taxonomies
+        //console.dir(Results.findOne({}).result.results[0].taxonomies)
+        return Results.findOne({}).result.results[0].taxonomies
     },
 });
 
@@ -69,46 +66,6 @@ Template.NPIsearch.events({
         //console.log(number)
         Meteor.call('NumberSearch', number)
         Session.set('NPISearchNumber', number)
-        Session.set('NPIresultPresent',true)
+        Session.set('resultPresent',true)
     },
 });
-
-Template.nameSearch.events({
-    'submit form'(event, template) {
-        event.preventDefault()
-        var first = event.target.first.value
-        var last = event.target.last.value
-        Meteor.call('nameSearch', first, last)
-        Session.set('firstName', first)
-        Session.set('lastName', last)
-        Session.set('nameResultPresent', true)
-    }
-})
-
-Template.nameResults.helpers({
-    nameResultPresent() {
-        return Session.get('nameResultPresent')
-    },
-    firstName() {
-        return Session.get('firstName')
-    },
-    lastName() {
-        return Session.get('lastName')
-    },
-    nameResult() {
-        names = Results.findOne({ type: 'name' }).result.results
-        console.dir(names)
-        return names
-    }
-})
-
-Template.nameResults.events({
-    'click .resultRow' (event, tempate){
-        event.preventDefault()
-        Meteor.call('NpiSearchFromClick', this.number)
-        Session.set('NPISearchNumber', this.number)
-        Session.set('NPIresultPresent', true)        
-        Meteor.call('NumberSearch', this.number)
-        Session.set('searchTypeName', false) //Returns to NPI search screen.
-    }
-})
--- server/main.js
+++ server/main.js
@@ -9,51 +9,17 @@ Meteor.startup(() => {
 
 Meteor.methods({
     NumberSearch: function (number) {
-        npiresult = HTTP.call(
+        result = HTTP.call(
             'GET', 'https://npiregistry.cms.hhs.gov/api', {
                 params: {
                     'number': number
                 }
             }).data
           // console.dir(result)
-        Meteor.call('clearNPIResults')
-        Results.insert({
-            type: 'npi',
-            result: npiresult
-        })// inserts new results into db for display.
+        Meteor.call('ClearResults')
+        Results.insert({ result })// inserts new results into db for display.
     },
-
-
-    nameSearch: function (first, last) {
-        nameresult = HTTP.call(
-            'GET', 'https://npiregistry.cms.hhs.gov/api', {
-                params: {
-                    'first_name': first,
-                    'last_name': last
-                }
-            }).data
-        Meteor.call('clearNameResults')
-        console.dir(nameresult)
-        Results.insert({
-            type: 'name',
-            result: nameresult
-        })
-        
+    ClearResults: function () {
+        Results.remove({}) // Remove any prior results. this is run on browser startup.
     },
-
-
-    clearNPIResults: function () {
-        console.log('npi result supposed to be cleared')
-        Results.remove({type: 'npi'}) // Remove any prior NPI results. this is run on browser startup.
-    },
-    clearNameResults: function () {
-        Results.remove({type: 'name'}) // Remove any prior Name results. this is run on browser startup.
-    },
-    clearResults: function () {
-        Results.remove({})
-    },
-
-    NpiSearchFromClick: function (number) {
-        console.log(number)
-    }
 })
